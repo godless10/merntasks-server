ac389c679e5743cd35d03e1dced33f420c0eeeb2
godless10 <59809881+godless10@users.noreply.github.com>
2020-03-20 09:32:54 -0500

    index

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json({ extended: true }));
 
 
 //puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -36,6 +36,6 @@ app.get('/', (req, res) =>{
 
 
 
-app.listen(PORT, () =>{
+app.listen(port,'0.0.0.0', () =>{
     console.log(`el server esta funcionando en el puerto ${PORT}`)
 })
